cd7ef81deea94be14692c09c1c2597ccfbb1fc23
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2025-07-17 12:42:14 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_SOTwx01jAWQi@ep-tight-king-a12temqu-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+    url: "postgresql://neondb_owner:npg_XGpJfq0HaUr5@ep-tight-king-a12temqu-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
