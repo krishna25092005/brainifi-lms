a21544e1d67db2d33513d37bbee859b1b1c2dab4
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2025-07-17 15:12:27 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING,
+    url: process.env.DATABASE_URL,
   },
 });
